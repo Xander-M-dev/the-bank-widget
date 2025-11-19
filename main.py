0b07f3db72e599a04867712ba6b94b3e38ce0f4d
александр <mr.Si280@mail.ru>
2025-11-20 00:45:56 +0300

    temp: current decorator work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+
 if __name__ == "__main__":
     print(get_mask_card_number.__doc__)
     print(get_mask_account.__doc__)
@@ -10,7 +11,6 @@ print(mask_account_card("Visa Platinum 7000792289606361"))  # Visa Platinum 7000
 print(mask_account_card("Maestro 7000792289606361"))  # Maestro 7000 79** **** 6361
 print(mask_account_card("Счет 73654108430135874305"))  # Счет **4305
 
-from src.widget import mask_account_card
 
 
 def main():
