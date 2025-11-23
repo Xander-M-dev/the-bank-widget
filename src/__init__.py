1bfe257025397e2954f26ec8368fb19f960a3bfe
александр <mr.Si280@mail.ru>
2025-11-23 15:36:03 +0300

    fix: исправлен импорт в __init__.py - дален старый импорт get_currency_rate - обавлен новый импорт convert_currency

--- src/__init__.py
+++ src/__init__.py
@@ -1,28 +1,19 @@
-"""The Bank Widget - банковский виджет для обработки операций."""
+"""Пакет The Bank Widget для обработки банковских операций."""
 
-__version__ = "0.1.0"
-__author__ = "Xander-M-dev <mixmary90@gmail.com>"
-
-from .decorators import log
-from .external_api import get_currency_rate, get_transaction_amount_rub
-from .generators import card_number_generator, filter_by_currency, transaction_descriptions
-from .masks import get_mask_account, get_mask_card_number
-from .processing import filter_by_state, sort_by_date
-from .utils import load_transactions
-from .widget import get_date, mask_account_card
+from . import masks
+from . import widget
+from . import processing
+from . import generators
+from . import decorators
+from . import utils
+from . import external_api
 
 __all__ = [
-    "get_mask_card_number",
-    "get_mask_account",
-    "mask_account_card",
-    "get_date",
-    "filter_by_state",
-    "sort_by_date",
-    "filter_by_currency",
-    "transaction_descriptions",
-    "card_number_generator",
-    "log",
-    "load_transactions",
-    "get_transaction_amount_rub",
-    "get_currency_rate",
+    'masks',
+    'widget',
+    'processing',
+    'generators',
+    'decorators',
+    'utils',
+    'external_api'
 ]
